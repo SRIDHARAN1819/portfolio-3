32be16f21f038b0281748f2fa1a117f9619bbefc
SRIDHARAN S <82379566+SRIDHARAN1819@users.noreply.github.com>
2023-01-28 12:53:29 +0530

    Add files via upload

---
+++ Personal-portfolio/script.js
@@ -0,0 +1,14 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["AI&DS Student","Front-End Developer", "AI Engineer","AI&DS Student","Web Designer", "Data Analyst"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["AI&DS Student", "Front-End Developer", "AI Engineer","AI&DS Student", "Web Designer", "Data Analyst"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
